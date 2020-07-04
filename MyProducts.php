9dd08256324ffa89db097c3225c93cf386b388ba
Kritanjali Jain <kritanjalijain@gmail.com>
2020-07-04 20:44:53 +0530

    Add files via upload

---
+++ MyProducts.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Modern Farmers</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+  
+</head>
+
+<body>
+   
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Farm Connect</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="#">Buy Farm Products</a>
+                    </li>
+					<li>
+                        <a href="#">Nearby Farms</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+	</br>
+	</br>
+	<hr>
+	
+	<table>
+		<tr>
+			<td>ID</td>
+         </tr>
+         <tr>   
+			<td>Category</td>
+        </tr>
+        <tr>
+			<td>Description</td>
+        </tr>
+        <tr>
+			<td>Price</td>
+            </tr>
+        <tr>   
+			<td>Date Added</td>
+		</tr>
+	</table>
+	
+	
+	
+	</body>
+
+
+
+
+</html>
---
+++ showImage.php
@@ -0,0 +1,20 @@
+<?php
+			$servername = "localhost";
+			$username = "root";
+			$password = "femi";
+			$dbname = "register";
+			$conn = mysqli_connect($servername, $username, $password);
+			mysqli_select_db($conn, $dbname);
+			if (isset($_GET['CompanyName'])){
+				$username = mysqli_real_escape_string($_GET['CompanyName']);
+				$query = mysqli_query("SELECT * FROM products WHERE 'CompanyName' = '$username'") ; 
+				while($row = mysql_fetch_assoc($query){
+					$imageData = $row["image"];
+				}
+				header("content-type: image/jpeg");
+				echo $imageData;
+			}
+			else{	
+			echo "Error!";
+			}
+?>
